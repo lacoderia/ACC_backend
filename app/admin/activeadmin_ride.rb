cee2d2adebc3b492e7b3d5fd023340bc29037ef4
tonklis <benjamin.hernandez@in2teck.com>
2014-09-12 18:36:16 -0500

    Se agregó selector para agregar usuarios en Rides.

--- app/admin/activeadmin_ride.rb
+++ app/admin/activeadmin_ride.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Ride do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :agreement, :user, :ride_when, :origin, :destination, :cost, :seats, :notes
+  permit_params :agreement, :users, :ride_when, :origin, :destination, :cost, :seats, :notes
   #
   # or
   #
@@ -13,6 +13,20 @@ ActiveAdmin.register Ride do
   #   permitted << :other if resource.something?
   #   permitted
   # end
+
+	form do |f|
+		f.inputs "Ride Details" do
+			f.input :agreement
+			f.input :users, :collection => User.all, :as => :select
+			f.input :ride_when
+			f.input :origin
+			f.input :destination
+			f.input :cost
+			f.input :seats
+			f.input :notes
+		end
+		f.actions
+	end
 
 	config.filters = false
 

54a0c7fc6c01198ea11d3febe1bfdf4d2b90c954
tonklis <benjamin.hernandez@in2teck.com>
2014-10-06 17:53:05 -0500

    Prueba para forzar a png las imágenes subidas

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
 	before_save :ensure_authentication_token
 
-  has_attached_file :avatar, :styles => { :medium => "200x200#", :thumb => "40x40#" }, :url => "/:class/:attachment/:id/:style.:extension", :default_url => "/:class/:attachment/default.png"
+  has_attached_file :avatar, :styles => { :medium => ["200x200#", :png], :thumb => ["40x40#", :png] }, :url => "/:class/:attachment/:id/:style.png", :default_url => "/:class/:attachment/default.png"
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
 	has_many :my_rides, :foreign_key => :user_id, :class_name => 'Ride' 

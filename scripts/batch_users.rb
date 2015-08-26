f9b278b448c4f9de4528b1bd8fadccf2bb8a2e97
tonklis <benjamin.hernandez@in2teck.com>
2015-08-26 13:23:43 -0500

    Users y Vehicles scripts para subir a production

--- scripts/batch_users.rb
+++ scripts/batch_users.rb
@@ -8,7 +8,8 @@ CSV.foreach(File.path("users.csv")) do |col|
   if user
     user.update_attribute(:is_member, true)
   else
-    password = [*('a'..'z'),*('0'..'9')].shuffle[0,8].join
+    #password = [*('a'..'z'),*('0'..'9')].shuffle[0,8].join
+    password = col[1] 
     email = col[4] ? col[4] : "#{col[0]}#{col[1]}@acc.com.co"
     user = User.create(document_type: col[0], document_id: col[1], first_name: col[2], last_name: col[3], email: email, is_member: true, active: true, agreement_id: nil, password: password, confirmation_token: nil, confirmed_at: Time.now)
     users << [col[0],col[1],password]
